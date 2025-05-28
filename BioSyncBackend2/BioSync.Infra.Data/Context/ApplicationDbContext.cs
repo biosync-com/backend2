8a2835758820a63c634d768fd54df3916340526b
joaotavares1 <joao.andrade30@fatec.sp.gov.br>
2025-05-28 11:31:41 -0300

    InfraData

---
+++ BioSyncBackend2/BioSync.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BioSync.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioSync.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+       
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Coletor> Coletores { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<Material> Materiais { get; set; }
+        public DbSet<PontoDescarte> PontosDescarte { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        }
+    }
+}
---
+++ BioSyncBackend2/BioSync.Infra.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BioSync.Infra.Data.Identity
+
+{
+    public class ApplicationUser
+    {
+        public string UserName { get; set; }
+
+        public string Senha {  get; set; }
+    }
+}
